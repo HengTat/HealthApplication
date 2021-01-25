2935e15223702afef171ac324b9456c426ed7ab3
Chonght <60685286+HengTat@users.noreply.github.com>
2021-01-25 11:46:26 +0800

    add update history

--- healthapp/src/components/Updates.js
+++ healthapp/src/components/Updates.js
@@ -1,82 +1,107 @@
-import react, { useEffect } from 'react'
+import react, { useEffect, useState } from 'react'
 import {MDBCard,MDBDataTable} from 'mdbreact'
 import PostHealthUdpates from './HealthUpdates/posthealthupdates';
 import axios from 'axios';
-
-const data = {
-  columns: [
-    {
-      label: "",
-      field: "number",
-      sort: "asc",
-      width: 150,
-    },
-    {
-      label: "Height",
-      field: "height",
-      sort: "asc",
-      width: 270,
-    },
-    {
-      label: "Weight",
-      field: "weight",
-      sort: "asc",
-      width: 270,
-    },
-    {
-      label: "BMI",
-      field: "description",
-      sort: "asc",
-      width: 270,
-    },
-    {
-      label: "Status",
-      field: "Status",
-      sort: "asc",
-      width: 270,
-    },
-  ],
-  rows: [],
-};
-
+import { useSerialIds } from 'highcharts';
 
 function Updates(props){
+  const [isLoading, setisLoading] = useState(true);
+  const [updatedata, setupdatedata] = useState([]);
+  
+  const data = {
+    columns: [
+      {
+        label: "Id",
+        field: "number",
+        sort: "asc",
+        width: 150,
+      },
+      {
+        label: "Height",
+        field: "height",
+        sort: "asc",
+        width: 270,
+      },
+      {
+        label: "Weight",
+        field: "weight",
+        sort: "asc",
+        width: 270,
+      },
+      {
+        label: "BMI",
+        field: "BMI",
+        sort: "asc",
+        width: 270,
+      },
+      {
+        label: "Status",
+        field: "status",
+        sort: "asc",
+        width: 270,
+      },
+    ],
+    rows: updatedata,
+  };
 
-  useEffect(()=>{
-    console.log("test");
-    axios.get("http://localhost:3000/updates/getcurrenthealthstatus/"+props.curremail).then(data=>data.rows);
-  })
+  useEffect(() => {
+    axios
+      .get(
+        "http://localhost:3000/updates/getcurrenthealthstatus/" +
+          props.curremail
+      )
+      .then((data) => {
+        var i=1;
+        for(var x of data.data){
+            x['number']=i;
+            i++;
+            x["BMI"]="test";
+            x["status"]="test";
+        }
+        setupdatedata(data.data);
+        data.rows = updatedata;
+        setisLoading(false);
+      });
+  },[]);
 
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
 
-    return (
-      <div>
-        <MDBCard
-          style={{
-            paddingTop: "40px",
-            AlignItems: "center",
-            backgroundColor: "#4B515D",
-            color: "white",
-          }}
-        >
-          <h2>HealthUpdates</h2>
-
-          <PostHealthUdpates style={{float:"right"}} curremail={props.curremail}></PostHealthUdpates>
-        </MDBCard>
-        <MDBCard style={{ backgroundColor: "#4B515D" }}>
-          <MDBDataTable
-            theadColor="darkgrey"
-            theadTextWhite
-            striped
-            bordered
-            small
-            fixed
-            entriesOptions={[1, 5, 10]}
-            entries={10}
-            data={data}
-          />
-        </MDBCard>
-      </div>
-    );
+  return (
+    <div>
+      <MDBCard
+        style={{
+          paddingTop: "40px",
+          AlignItems: "center",
+          backgroundColor: "#4B515D",
+          color: "white",
+        }}
+      >
+        <h2>HealthUpdates</h2>
+        <PostHealthUdpates
+          style={{ float: "right" }}
+          curremail={props.curremail}
+        ></PostHealthUdpates>
+      </MDBCard>
+      <MDBCard style={{ backgroundColor: "#4B515D" }}>
+        <MDBDataTable
+          dark
+          theadColor="darkgrey"
+          theadTextWhite
+          striped
+          bordered
+          small
+          fixed
+          hover
+          tbodyTextWhite
+          entriesOptions={[1, 5, 10]}
+          entries={10}
+          data={data}
+        />
+      </MDBCard>
+    </div>
+  );
 }
 
 export default Updates;
